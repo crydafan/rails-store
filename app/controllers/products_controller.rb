0680120efd1f1107a70dbb24cf2d75f3be6ddf78
Leonardo <leonardo.estacio.h@gmail.com>
2026-06-20 14:15:03 -0500

    feat: add inventory count to product form and validate it

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -46,7 +46,7 @@ class ProductsController < ApplicationController
   end
 
   def product_params
-    params.expect(product: [ :name, :description, :featured_image ])
+    params.expect(product: [ :name, :description, :featured_image, :inventory_count ])
   end
 
   def switch_locale(&action)
